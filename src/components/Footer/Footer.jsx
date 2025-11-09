417daa0d2f88297e4d65188574eb2d8646646bd3
rafi0012 <rafiultalukdar007@gmail.com>
2025-11-09 12:36:24 +0600

    Added Footer Section

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { BsTwitterX } from 'react-icons/bs';
+import { FaFacebookSquare, FaInstagram, FaPhoneVolume, FaRegCalendar } from 'react-icons/fa';
+import { IoIosMail } from 'react-icons/io';
+import { IoLocation } from 'react-icons/io5';
+
+const Footer = () => {
+    return (
+        <>
+            <div className='bg-[#FBFCFB]'>
+                <div className='container'>
+                    <div className='py-[35px] grid grid-cols-12 gap-5 gap-y-[30px] border-b border-[#219E64]'>
+                        <div className='col-span-12 sm:col-span-6 lg:col-span-4 max-w-[320px]'>
+                            <button className='flex items-center gap-1.5 text-[20px] sm:text-[22px] font-bold text-[#219E64]'><FaRegCalendar /><span>EventSphere</span></button>
+                            <p className='text-[18px] text-black pt-5'>EventSphere brings energy, elegance, and unforgettable moments to every celebration, making each event truly special and memorable.</p>
+                        </div>
+                        <div className='col-span-12 sm:col-span-6 lg:col-span-3'>
+                            <h4 className='text-[21px] text-black font-semibold pb-[15px]'>Quick Links</h4>
+                            <ul className='flex flex-col gap-3'>
+                                <li className='text-[18px] text-[#141414] font-medium'>Services</li>
+                                <li className='text-[18px] text-[#141414] font-medium'>Contact Us</li>
+                                <li className='text-[18px] text-[#141414] font-medium'>Privacy Policy</li>
+                            </ul>
+                        </div>
+                        <div className='col-span-12 sm:col-span-6 lg:col-span-3'>
+                            <h4 className='text-[21px] text-black font-semibold pb-[15px]'>Contact Us</h4>
+                            <ul className='flex flex-col gap-3'>
+                                <li className='text-[18px] text-[#141414] font-medium flex items-center gap-[7px]'><IoIosMail /><span>info@eventsphere.com</span></li>
+                                <li className='text-[18px] text-[#141414] font-medium flex items-center gap-[7px]'><FaPhoneVolume /><span>+880 1XXX-XXXXXX</span></li>
+                                <li className='text-[18px] text-[#141414] font-medium flex items-center gap-[7px]'><IoLocation /><span>Dhaka, Bangladesh</span></li>
+                            </ul>
+                        </div>
+                        <div className='col-span-12 sm:col-span-6 lg:col-span-2'>
+                            <h4 className='text-[21px] text-black font-semibold pb-[15px]'>Follow Us</h4>
+                            <ul className='flex items-center gap-6'>
+                                <li className='text-[#219E64] hover:text-[#069251] duration-300 cursor-pointer'><FaInstagram size={30} /></li>
+                                <li className='text-[#219E64] hover:text-[#069251] duration-300 cursor-pointer'><FaFacebookSquare size={30} /></li>
+                                <li className='text-[#219E64] hover:text-[#069251] duration-300 cursor-pointer'><BsTwitterX size={28} /></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className='py-7 text-center'>
+                        <p className='text-[#141414] text-[16px]'>© 2025 EventSphere. All rights reserved.</p>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Footer;
--- src/components/layouts/Root.jsx
+++ src/components/layouts/Root.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import Navbar from '../Navbar/Navbar';
 import { Outlet } from 'react-router';
+import Footer from '../Footer/Footer';
 
 const Root = () => {
     return (
-        <div>
+        <>
             <Navbar></Navbar>
             <Outlet></Outlet>
-        </div>
+            <Footer></Footer>
+        </>
     );
 };
 
